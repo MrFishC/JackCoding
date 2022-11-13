7deafd0a2d6966114ba4385645bb9434925d5b50
MrFishC <yucrun@163.com>
2022-11-13 15:30:51 +0800

    欢迎页面使用lottie库

--- business/module_login/src/main/java/cn/jack/module_login/mvvm/view/SplashActivity.kt
+++ business/module_login/src/main/java/cn/jack/module_login/mvvm/view/SplashActivity.kt
@@ -1,24 +1,56 @@
 package cn.jack.module_login.mvvm.view
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
 import android.annotation.SuppressLint
 import cn.jack.module_login.databinding.ModuleLoginActivitysPlashBinding
 import com.jack.lib_base.base.view.BaseSimpleActivity
 import com.jack.lib_base.ext.launchActivity
 
 @SuppressLint("CustomSplashScreen")
-class SplashActivity : BaseSimpleActivity<ModuleLoginActivitysPlashBinding>(ModuleLoginActivitysPlashBinding::inflate) {
+class SplashActivity :
+    BaseSimpleActivity<ModuleLoginActivitysPlashBinding>(ModuleLoginActivitysPlashBinding::inflate) {
     override fun prepareData() {
         super.prepareData()
 
-        mBinding.ferrisWheelView.startAnimation()
+//        mBinding.ferrisWheelView.startAnimation()
+        startAnimation()
+
+//        val duration: Long = mBinding.splashAnimation.duration
+
         postDelayed({
             launchActivity(LoginActivity::class.java)
             finish()
         }, 3000)
     }
 
+    private fun startAnimation() {
+//        mBinding.splashAnimation.setAnimation("splash_loading.json")
+        mBinding.splashAnimation.setAnimation("lottie_animation.json")
+        mBinding.splashAnimation.addAnimatorListener(object : AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                println("onAnimationEnd ")
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+        })
+        mBinding.splashAnimation.playAnimation()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
-        mBinding.ferrisWheelView.stopAnimation()
+//        mBinding.ferrisWheelView.stopAnimation()
+
+        //参考：https://juejin.cn/post/6890861185953251341
+        //在测试过程中，发现 animation.cancelAnimation()，最终也会回调 onAnimationEnd这个方法。
+        //读过源码之后，发现确实是在cancel之后回调了animationEnd的方法。
+        mBinding.splashAnimation.cancelAnimation()
     }
 }
